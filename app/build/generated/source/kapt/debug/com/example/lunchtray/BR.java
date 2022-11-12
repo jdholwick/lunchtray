88aab386f8fbef3150f36f1d79539f91ff7e63c8
J.D. Holwick <70964794+jdholwick@users.noreply.github.com>
2022-11-12 16:42:02 -0700

    initial commit

---
+++ app/build/generated/source/kapt/debug/com/example/lunchtray/BR.java
@@ -0,0 +1,11 @@
+package com.example.lunchtray;
+
+public class BR {
+  public static final int EntreeMenuFragment = 1;
+
+  public static final int SideMenuFragment = 2;
+
+  public static final int _all = 0;
+
+  public static final int viewModel = 3;
+}
